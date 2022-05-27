5f505932cd0a3c93898afc50d312e73b10cafe4f
Austin Akers <austin.akers5@gmail.com>
2022-05-27 00:33:20 -0700

    added ethereum address validation to new contact popover

--- ui/components/app/add-new-contact-popover/add-new-contact-popover.component.js
+++ ui/components/app/add-new-contact-popover/add-new-contact-popover.component.js
@@ -1,12 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../ui/button';
 import Popover from '../../ui/popover';
 import TextField from '../../ui/text-field';
 import { ENVIRONMENT_TYPE_FULLSCREEN } from '../../../../shared/constants/app';
 import { getEnvironmentType } from '../../../../app/scripts/lib/util';
+import { isValidAddress } from 'ethereumjs-util';
+import { providers } from 'ethers';
+import { debounce } from 'lodash';
 
 import { I18nContext } from '../../../contexts/i18n';
+import { isValidDomainName } from '../../../helpers/utils/util';
+import { isBurnAddress } from '../../../../shared/modules/hexstring-utils';
 
 const environmentType = getEnvironmentType();
 
@@ -26,7 +31,7 @@ const AddNewContactPopoverFooter = (props) => {
         }}
         rounded
       >
-        {t('canel')}
+        {t('cancel')}
       </Button>
       <Button
         type="primary"
@@ -48,35 +53,70 @@ const AddNewContactPopover = (props) => {
 
   const [username, setUsername] = useState('');
   const [ethereumAddress, setEthereumAddress] = useState('');
+  const [ethereumAddressError, setEthereumAddressError] = useState('');
   const [memo, setMemo] = useState('');
+
+  const validateAddress = async (address) => {
+    const ethAddress = address.trim();
+    const validEthAddress = isValidAddress(ethAddress);
+    const validEnsAddress =
+      isValidDomainName(ethAddress) && /\.eth$/u.test(ethAddress);
+
+    if (validEthAddress) {
+      if (isBurnAddress(ethAddress)) {
+        setEthereumAddress(address);
+        setEthereumAddressError(t('burnAddress'));
+      } else {
+        setEthereumAddress(address);
+        setEthereumAddressError('');
+      }
+    } else if (validEnsAddress) {
+      const provider = new providers.Web3Provider(global.ethereumProvider);
+      try {
+        const ensHexAddress = await provider.resolveName(ethAddress);
+        setEthereumAddressError('');
+        setEthereumAddress(ensHexAddress);
+        setUsername((name) => name || ethAddress);
+      } catch (error) {
+        setEthereumAddressError(error.message);
+      }
+    } else {
+      setEthereumAddressError(t('invalidAddress'));
+      setEthereumAddress(address);
+    }
+  };
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
 
   const handleEthereumAddressChange = (event) => {
-    setEthereumAddress(event.target.value);
+    validateAddress(event.target.value);
   };
 
   const handleMemoChange = (event) => {
     setMemo(event.target.value);
-  }
+  };
 
   const { isOpen } = props;
   return (
     <Popover footer={<AddNewContactPopoverFooter isOpen={isOpen} />}>
       <div className="add-new-contact-popover__container">
-        <h2 className="add-new-contact-popover__header__title">{t('newContact')}</h2>
-        <p className="add-new-contact-popover__header__subtitle">Enter contact details.</p>
+        <h2 className="add-new-contact-popover__header__title">
+          {t('newContact')}
+        </h2>
+        <p className="add-new-contact-popover__header__subtitle">
+          {t('newContactSubtitle')}
+        </p>
         <form>
           <div className="add-new-contact-popover__group">
             <label className="address-book__view-contact__group__label">
-              {t('username')}
+              {t('userName')}
             </label>
             <TextField
               value={username}
               onChange={handleUsernameChange}
-              placeholder={isFullScreen ? '' : t('username')}
+              placeholder={isFullScreen ? '' : t('userName')}
             />
           </div>
 
@@ -89,6 +129,11 @@ const AddNewContactPopover = (props) => {
               onChange={handleEthereumAddressChange}
               placeholder={isFullScreen ? '' : t('addAnEthereumAddress')}
             />
+            {ethereumAddressError && (
+              <div className="address-book__add-contact__error">
+                {ethereumAddressError}
+              </div>
+            )}
           </div>
 
           <div className="add-new-contact-popover__group">
